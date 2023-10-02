9572509446fa4044596e20c21295b22a87ccd6cd
tracer-ics2021 <tracer@njuics.org>
2023-10-02 21:35:18 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 4c1b61708d134deebf3aa552fba086cf   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  21:35:18 up 1 day, 11:16,  1 user,  load average: 0.13, 0.06, 0.01

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -3,7 +3,9 @@
 NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
-  //int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
-  //  (nemu_state.state == NEMU_QUIT);
-  return true;
+  if (nemu_state.state == NEMU_STOP)
+    printf("fuck!!\n");
+  int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
+    (nemu_state.state == NEMU_QUIT);
+  return !good;
 }
