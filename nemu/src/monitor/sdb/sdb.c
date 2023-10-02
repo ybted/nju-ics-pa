a6bd883b69c7cbad4e3ead203f19edd1cf9b7837
tracer-ics2021 <tracer@njuics.org>
2023-10-02 23:17:33 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 4c1b61708d134deebf3aa552fba086cf   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  23:17:33 up 1 day, 12:58,  1 user,  load average: 0.10, 0.08, 0.03

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -51,7 +51,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si [N]", "Execute N instructions", cmd_si }
+  { "si", "Execute N instructions", cmd_si }
 
   /* TODO: Add more commands */
 
