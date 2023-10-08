3aac324d7e12b28048e7a6d17e3e483b18d267ed
tracer-ics2021 <tracer@njuics.org>
2023-10-08 10:49:00 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 04c59ec2b7024f969ceaf953af5f1046   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  10:49:00 up 2 days, 18:41,  1 user,  load average: 0.22, 0.14, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -180,8 +180,8 @@ word_t expr(char *e, bool *success) {
       if (tokens[i+1].type == TK_REG)
       {
         paddr_t addr = 0;
-        sscanf(tokens[i+1].str, "%x", &addr);
-        printf("addr: %x", addr);
+        sscanf(tokens[i+1].str, "%d", &addr);
+        printf("addr: %x\n", addr);
         word_t val = paddr_read(addr, 4);
         tokens[i+1].type = TK_NUM;
         sprintf(tokens[i+1].str, "%u", val); 
