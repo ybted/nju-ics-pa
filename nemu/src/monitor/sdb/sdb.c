dc0c23c5fd602502f0b263f5c8facdd2d90d38c8
tracer-ics2021 <tracer@njuics.org>
2023-10-09 08:28:27 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 04c59ec2b7024f969ceaf953af5f1046   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  08:28:27 up 3 days, 16:20,  1 user,  load average: 0.39, 0.32, 0.21

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -154,6 +154,7 @@ static int cmd_info(char *args)
   } else if (arg[0] == 'w') {
     WP* p = head;
     int index = 0;
+    printf("I'm here!\n");
     while (p) {
       printf("index: %d expr: %s \n", index ++, p->expr);
       p = p->next;
