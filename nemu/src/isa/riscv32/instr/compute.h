8f640c4418d1acc483f2e777b026faba43d7ac79
tracer-ics2021 <tracer@njuics.org>
2023-11-02 13:14:38 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 6c4030aaafdd4cd192e6329b0b285c2f   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  13:14:38 up 3 min,  1 user,  load average: 0.19, 0.09, 0.03

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -1,15 +1,15 @@
 def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
-  printf("DEBUG lui: %x\n", *ddest);
+  //printf("DEBUG lui: %x\n", *ddest);
 }
 
 def_EHelper(addi) {
   //printf("DEBUF addi %x %x\n", *id_src1->preg, id_src2->imm);
   rtl_addi(s, ddest, id_src1->preg, id_src2->imm);
-  printf("DEBUG addi: %x\n", *ddest);
+  //printf("DEBUG addi: %x\n", *ddest);
 }
 
 def_EHelper(auipc) {
   rtl_li(s, ddest, id_src1->imm + s->pc);
-  printf("DEBUG auipc: %x\n", *ddest);
+  //printf("DEBUG auipc: %x\n", *ddest);
 }
--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -1,11 +1,11 @@
 def_EHelper(jal) {
   rtl_li(s, ddest, s->snpc);
-  printf("DEBUG jal: %x %x\n", id_src1->imm, s->pc);
+  //printf("DEBUG jal: %x %x\n", id_src1->imm, s->pc);
   rtl_j(s, id_src1->imm + s->pc);
 }
 
 def_EHelper(jalr) {
   rtl_li(s, ddest, s->snpc);
-  printf("DEBUF jalr: %x %x %x\n", *id_src1->preg, id_src2->imm, s->pc);
+  //printf("DEBUF jalr: %x %x %x\n", *id_src1->preg, id_src2->imm, s->pc);
   rtl_j(s,  *id_src1->preg);
 }
