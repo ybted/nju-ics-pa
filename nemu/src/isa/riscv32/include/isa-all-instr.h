89a5cb2b69f0ae67229da424419fc6a3d829d867
tracer-ics2021 <tracer@njuics.org>
2023-11-03 11:08:05 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: eeb82da55ef24cfabcd24bb26549a874   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  11:08:05 up 17:43,  1 user,  load average: 0.12, 0.14, 0.11

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,6 +1,6 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(lui) f(lw) f(lbu) f(srai) f(sw) f(sh) f(inv) f(addi) f(nemu_trap) f(auipc) f(jal) f(jalr) f(add) f(sub) f(sll) f(and) f(sltiu) f(andi) f(xori) f(beq) f(bne) f(sltu) f(xor) f(or) 
+#define INSTR_LIST(f) f(lui) f(lw) f(lbu) f(srai) f(sb) f(sw) f(sh) f(inv) f(addi) f(nemu_trap) f(auipc) f(jal) f(jalr) f(add) f(sub) f(sll) f(and) f(sltiu) f(andi) f(xori) f(beq) f(bne) f(sltu) f(xor) f(or) 
  
 def_all_EXEC_ID();
