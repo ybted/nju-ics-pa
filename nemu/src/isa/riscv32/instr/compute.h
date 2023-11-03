f089f17a8aacdd28f832948507cbafd96a5c397e
tracer-ics2021 <tracer@njuics.org>
2023-11-03 09:22:37 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: eeb82da55ef24cfabcd24bb26549a874   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  09:22:37 up 15:57,  1 user,  load average: 0.08, 0.06, 0.06

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -4,9 +4,9 @@ def_EHelper(lui) {
 }
 
 def_EHelper(addi) {
-  printf("DEBUF addi: %u %u %u\n", *ddest, *dsrc1, id_src2->imm);
+  //printf("DEBUF addi: %u %u %u\n", *ddest, *dsrc1, id_src2->imm);
   rtl_addi(s, ddest, dsrc1, id_src2->imm);
-  printf("DEBUF addi: %u %u %u\n", *ddest, *dsrc1, id_src2->imm);
+  //printf("DEBUF addi: %u %u %u\n", *ddest, *dsrc1, id_src2->imm);
 }
 
 def_EHelper(sltiu) {
