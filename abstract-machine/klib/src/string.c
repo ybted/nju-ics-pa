6130a6dc3765d29a7b8090aadca4db1dde15dd5b
tracer-ics2021 <tracer@njuics.org>
2023-11-03 18:21:23 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 5685f9902def4aa28a94eeed35b461dd   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  18:21:23 up  2:43,  1 user,  load average: 0.04, 0.03, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -49,6 +49,7 @@ void *memset(void *s, int c, size_t n) {
   unsigned char *xs =  s;
   while (n--) 
     *xs++ = c;
+
   return s;
 }
 
@@ -68,17 +69,15 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  if (s1 == NULL || s2 == NULL) {
-    return 0;
+  assert(s1 && s2 && n >= 0);
+  const char* p1 = (char*)s1;
+  const char* p2 = (char*)s2;
+
+  while (n -- && *p1 && *p2 && (*p1 == *p2)) {
+      p1 ++;
+      p2 ++; 
   }
-  const unsigned char *src1 = s1;
-  const unsigned char *src2 = s2;
-  while (n != 0 && *src1 != '\0' && *src2 != '\0' && *src1 == *src2) {
-    --n;
-    ++src1;
-    ++src2;
-  }
-  return *src1 == *src2 || n == 0 ? 0 : *src1 < *src2 ? -1 : 1;
+  return *(const unsigned char*)p1 - *(const unsigned char*)p2;
 }
 
 #endif
