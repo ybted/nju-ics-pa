9ac3529bfa7f268347450f1ec021cc8209ea5956
tracer-ics2021 <tracer@njuics.org>
2023-11-03 19:47:26 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 7a2c50d0a2ce44428c029d5ae8dad25a   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  19:47:26 up 45 min,  1 user,  load average: 0.11, 0.07, 0.02

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,14 +7,64 @@
 
 int printf(const char *fmt, ...) {
   panic("Not implemented");
+  
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+int to_string(char* out, int num) {
+  int len = 0;
+  if (num < 0) {
+    *out++ = '-';
+    num *= -1;
+    len ++;
+  }
+  int tmp = 0;
+  int tmpnum = num;
+  while (tmpnum) {
+    tmpnum /= 10;
+    tmp ++;
+  }
+  for (int i = tmp; i > 0; i --) {
+    int base = 1;
+    for (int j = 0; j < i; j ++)
+      base *= 10;
+    *out++ = num % base + '0';
+    num -= (num % base) * base;
+  }
+  len += tmp;
+  return len;
+}
+
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+  int len = 0;
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt ++;
+      if (*fmt == 'd') {
+        int num = va_arg(args, int);
+        int inc = to_string(out, num);
+        len += inc;
+      } else if (*fmt == 's') {
+        char* str = va_arg(args, char*);
+        int tmp = strlen(str);
+        len += tmp;
+        for (int i = 0; i < tmp; i ++){
+          *out ++ = str[i];
+        }
+      }
+    } else {
+      *out++ = *fmt++;
+      len ++;
+    }
+  }
+  *out = '\0';
+  va_end(args);
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
