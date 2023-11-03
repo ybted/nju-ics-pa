068d25ba8b940db40d17c19d56c9fa49c821b0de
tracer-ics2021 <tracer@njuics.org>
2023-11-03 18:21:56 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 5685f9902def4aa28a94eeed35b461dd   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  18:21:56 up  2:43,  1 user,  load average: 0.09, 0.04, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -77,7 +77,7 @@ int memcmp(const void *s1, const void *s2, size_t n) {
       p1 ++;
       p2 ++; 
   }
-  return *(const unsigned char*)p1 - *(const unsigned char*)p2;
+  return n == 0 ? 0 : *(const unsigned char*)p1 - *(const unsigned char*)p2;
 }
 
 #endif
