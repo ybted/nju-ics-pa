dc740eb99bb3cb4f9d7bccbc92c2f6ccb666a257
tracer-ics2021 <tracer@njuics.org>
2023-11-09 15:13:38 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 2c9be6a437264cb3910761145ddad6d8   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  15:13:38 up 22:34,  1 user,  load average: 0.07, 0.06, 0.01

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -25,6 +25,17 @@ def_EHelper(jalr) {
   rtl_j(s,  *id_src1->preg);
   
   #ifdef CONFIG_FTRACE
+  for (int i = 0; i < 1024; ++i) {
+    if(elf_func[i].func_name[0] == '\0') {
+      break;
+    }
+    if (elf_func[i].start == id_src1->imm + s->pc) {
+      for (int i = 0; i < space; i ++)
+        printf(" ");
+      printf("%x: call [%s@0x%lx]\n", s->pc, elf_func[i].func_name, elf_func[i].start);
+      space += 4;
+    }
+  }
   if (s->isa.instr.i.rs1 == 1 && s->isa.instr.i.rd == 0)
   {
     space -= 4;
