8a9f4d15acb1db1756ae613923a2788dc811509d
tracer-ics2021 <tracer@njuics.org>
2023-11-09 11:33:33 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 2c9be6a437264cb3910761145ddad6d8   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  11:33:33 up 18:54,  1 user,  load average: 0.07, 0.04, 0.04

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -9,9 +9,6 @@ int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  // for (int i = 0 ; i < argc; i ++) {
-  //   printf("%s\n", argv[i]);
-  // }
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
 #else
