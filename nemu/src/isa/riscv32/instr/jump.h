c76f781c97a8ccab2d0f08700655f496ecd3f52c
tracer-ics2021 <tracer@njuics.org>
2023-11-09 15:16:51 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 2c9be6a437264cb3910761145ddad6d8   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  15:16:51 up 22:37,  1 user,  load average: 0.18, 0.11, 0.03

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -6,8 +6,8 @@ def_EHelper(jal) {
       break;
     }
     if (elf_func[i].start == id_src1->imm + s->pc) {
-      // for (int i = 0; i < space; i ++)
-      //   printf(" ");
+      for (int i = 0; i < space; i ++)
+        printf(" ");
       printf("%x: call [%s@0x%lx]\n", s->pc, elf_func[i].func_name, elf_func[i].start);
       space += 2;
     }
@@ -30,8 +30,8 @@ def_EHelper(jalr) {
       break;
     }
     if (elf_func[i].start == *id_src1->preg) {
-      // for (int i = 0; i < space; i ++)
-      //   printf(" ");
+      for (int i = 0; i < space; i ++)
+        printf(" ");
       printf("%x: call [%s@0x%lx]\n", s->pc, elf_func[i].func_name, elf_func[i].start);
       space += 2;
     }
@@ -39,16 +39,14 @@ def_EHelper(jalr) {
   if (s->isa.instr.i.rs1 == 1 && s->isa.instr.i.rd == 0)
   {
     space -= 2;
-    // for (int i = 0; i < space; i ++)
-    //   printf(" ");
+    for (int i = 0; i < space; i ++)
+      printf(" ");
     for (int i = 0; i < 1024; ++i) {
       if(elf_func[i].func_name[0] == '\0') {
         break;
       }
       if (elf_func[i].start <= *id_src1->preg && *id_src1->preg <= elf_func[i].start + elf_func[i].size) {
        
-        for (int i = 0; i < space; i ++)
-          printf(" ");
         printf("%x: ret [%s]\n", s->pc, elf_func[i].func_name);
       }
     }
