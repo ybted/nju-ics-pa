a9ce9985d05f1bbcbf001b1bb26faf087076edfc
tracer-ics2021 <tracer@njuics.org>
2023-11-09 10:05:59 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 2c9be6a437264cb3910761145ddad6d8   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  10:05:59 up 17:26,  1 user,  load average: 0.13, 0.08, 0.03

--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -5,9 +5,10 @@ void sdb_mainloop();
 void engine_start() {
 #ifdef CONFIG_TARGET_AM
   cpu_exec(-1);
-  assert(0);
+ 
 #else
   /* Receive commands from user. */
   sdb_mainloop();
+  printf("hhh\n");
 #endif
 }
