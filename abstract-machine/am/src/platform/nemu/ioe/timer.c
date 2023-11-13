8a23797928da8a30d126942cf30280245717b3cc
tracer-ics2021 <tracer@njuics.org>
2023-11-13 14:26:45 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: da5e9a6afca24a1bb3d0121822bd7ad1   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  14:26:46 up 1 day, 22:04,  1 user,  load average: 0.23, 0.21, 0.12

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,7 +6,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = 0;
-  outb(RTC_ADDR, uptime->us);
+  io_write(AM_TIMER_UPTIME, uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
