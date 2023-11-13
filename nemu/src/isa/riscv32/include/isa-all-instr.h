6baf42d52b6cd26709c970af6ce0a6c8c10a8cde
tracer-ics2021 <tracer@njuics.org>
2023-11-13 16:29:02 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: da5e9a6afca24a1bb3d0121822bd7ad1   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  16:29:02 up 2 days, 6 min,  1 user,  load average: 0.06, 0.10, 0.09

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,6 +1,8 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(bltu) f(srl) f(sra) f(divu) f(mulh) f(lhu) f(lh) f(blt) f(lb) f(slt) f(div) f(rem) f(remu) f(mul) f(bgeu) f(slli) f(lui) f(lw) f(lbu) f(srai) f(sb) f(sw) f(sh) f(inv) f(addi) f(srli) f(nemu_trap) f(auipc) f(jal) f(jalr) f(add) f(sub) f(sll) f(and) f(sltiu) f(andi) f(xori) f(beq) f(bne) f(bge) f(sltu) f(xor) f(or) 
+#define INSTR_LIST(f) \
+                      f(bltu) f(srl) f(sra) f(divu)\
+                      f(mulh) f(lhu) f(lh) f(blt) f(lb) f(slt) f(div) f(rem) f(remu) f(mul) f(bgeu) f(slli) f(lui) f(lw) f(lbu) f(srai) f(sb) f(sw) f(sh) f(inv) f(addi) f(srli) f(nemu_trap) f(auipc) f(jal) f(jalr) f(add) f(sub) f(sll) f(and) f(sltiu) f(andi) f(xori) f(beq) f(bne) f(bge) f(sltu) f(xor) f(or) 
  
 def_all_EXEC_ID();
