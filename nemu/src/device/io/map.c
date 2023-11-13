b2eba3d4af006cb669101e9a5560e093458c64c3
tracer-ics2021 <tracer@njuics.org>
2023-11-13 17:37:01 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: da5e9a6afca24a1bb3d0121822bd7ad1   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  17:37:01 up 2 days,  1:14,  1 user,  load average: 0.02, 0.06, 0.08

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -38,9 +38,6 @@ void init_map() {
 }
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
-  #ifdef CONFIG_DEVICE
-  printf("call device %s\n", map->name);
-  #endif
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
