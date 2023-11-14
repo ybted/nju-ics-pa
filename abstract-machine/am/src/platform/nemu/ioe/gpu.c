abe882f093371a1fbd3cf1b75911465c728c6af3
tracer-ics2021 <tracer@njuics.org>
2023-11-14 11:19:18 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: da5e9a6afca24a1bb3d0121822bd7ad1   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  11:19:18 up 2 days, 18:57,  1 user,  load average: 0.82, 0.45, 0.23

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -6,7 +6,7 @@
 void __am_gpu_init() {
   int i;
   int w = 350;  // TODO: get the correct width
-  int h = 341;  // TODO: get the correct height
+  int h = 343;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
