c834beb916e173154d227f14259f4cf09d3f8666
tracer-ics2021 <tracer@njuics.org>
2023-11-16 14:56:46 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: da5e9a6afca24a1bb3d0121822bd7ad1   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  14:56:46 up 4 days, 22:34,  1 user,  load average: 0.01, 0.07, 0.13

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,11 +5,11 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  for (int i = 0; i < 32; i ++)
-    printf("%d \n", c->gpr[i]);
-  printf("\n");
-  printf("mcause: %d\n", c->mcause);
-  printf("mstatus: %d\n", c->mstatus);
+  // for (int i = 0; i < 32; i ++)
+  //   printf("%d \n", c->gpr[i]);
+  // printf("\n");
+  // printf("mcause: %d\n", c->mcause);
+  // printf("mstatus: %d\n", c->mstatus);
   printf("mepc: %d\n", c->mepc);
   if (user_handler) {
     Event ev = {0};
