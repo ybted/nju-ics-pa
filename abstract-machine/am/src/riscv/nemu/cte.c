3dc400ad856cd8a8c89946f7b76ae56d1762e07c
tracer-ics2021 <tracer@njuics.org>
2023-11-16 14:51:17 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: da5e9a6afca24a1bb3d0121822bd7ad1   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  14:51:17 up 4 days, 22:29,  1 user,  load average: 0.11, 0.16, 0.17

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -10,7 +10,7 @@ Context* __am_irq_handle(Context *c) {
   printf("\n");
   printf("mcause: %d\n", c->mcause);
   printf("mstatus: %d\n", c->mstatus);
-  printf("mepc: %d %d\n", c->mepc);
+  printf("mepc: %d\n", c->mepc);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
