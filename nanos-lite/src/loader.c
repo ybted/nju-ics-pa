234f2b5ede872e7c122b31a0a7f0c3777ae98ab5
tracer-ics2021 <tracer@njuics.org>
2023-11-19 22:01:19 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: da5e9a6afca24a1bb3d0121822bd7ad1   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  22:01:19 up 8 days,  5:39,  1 user,  load average: 0.19, 0.29, 0.18

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,28 +14,35 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf;
   // Elf_Phdr ph;
   ramdisk_read(&elf, 0, sizeof(Elf_Ehdr) * 10);
-  printf("elf.e_type: %d\n", (int)elf.e_type);
-  printf("elf.e_machine: %d\n", (int)elf.e_machine);
-  printf("elf.e_version: %d\n", (int)elf.e_version);
-  printf("elf.e_entry: %d\n", (int)elf.e_entry);
-  printf("elf.e_phoff: %d\n", (int)elf.e_phoff);
-  printf("elf.e_shoff: %d\n", (int)elf.e_shoff);
-  printf("elf.e_flags: %d\n", (int)elf.e_flags);
-  printf("elf.e_ehsize: %d\n", (int)elf.e_ehsize);
-  printf("elf.e_phentize: %d\n", (int)elf.e_phentsize);
-  printf("elf.e_phnum: %d\n", (int)elf.e_phnum);
-  printf("elf.e_shentsize: %d\n", (int)elf.e_shentsize);
-  printf("elf.e_shnum: %d\n", (int)elf.e_shnum);
-  printf("elf.e_shstrndx: %d\n", (int)elf.e_shstrndx);
-  assert(elf.e_phoff != 0);
-  // ramdisk_read(&ph, sizeof(Elf_Ehdr), sizeof(Elf_Phdr));
+  // printf("elf.e_type: %d\n", (int)elf.e_type);
+  // printf("elf.e_machine: %d\n", (int)elf.e_machine);
+  // printf("elf.e_version: %d\n", (int)elf.e_version);
+  // printf("elf.e_entry: %d\n", (int)elf.e_entry);
+  // printf("elf.e_phoff: %d\n", (int)elf.e_phoff);
+  // printf("elf.e_shoff: %d\n", (int)elf.e_shoff);
+  // printf("elf.e_flags: %d\n", (int)elf.e_flags);
+  // printf("elf.e_ehsize: %d\n", (int)elf.e_ehsize);
+  // printf("elf.e_phentize: %d\n", (int)elf.e_phentsize);
+  // printf("elf.e_phnum: %d\n", (int)elf.e_phnum);
+  // printf("elf.e_shentsize: %d\n", (int)elf.e_shentsize);
+  // printf("elf.e_shnum: %d\n", (int)elf.e_shnum);
+  // printf("elf.e_shstrndx: %d\n", (int)elf.e_shstrndx);
+  // assert(elf.e_phoff != 0);
+  int num = elf.e_phnum;
+  int offset = elf.e_phoff;
+  int size = elf.e_phentsize;
+  Elf_Phdr ph[num];
+  for (int i = 0; i < num; i ++) {
+    ramdisk_read(&ph[i], offset + i * size, size);
+  }
+  
   
   assert(elf.e_ident[0] == 0x7f &&
          elf.e_ident[1] == 0x45 &&
          elf.e_ident[2] == 0x4c &&
          elf.e_ident[3] == 0x46 );
   // printf("ph.p_type: %d\n", ph.p_type);
-  // assert(ph.p_flags == (PF_X | PF_W | PF_R));
+  assert(ph[0].p_flags == PF_R);
   // ramdisk_read(pf + VirtualAddress, start_of_load,Memsize);
   return 0;
 }
