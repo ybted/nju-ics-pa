a57893242e2853d32b53f296dcfdff79d8750562
tracer-ics2021 <tracer@njuics.org>
2023-11-19 17:10:44 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: da5e9a6afca24a1bb3d0121822bd7ad1   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  17:10:44 up 8 days, 48 min,  1 user,  load average: 0.02, 0.10, 0.09

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,21 +12,21 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf;
-  Elf_Phdr ph;
+  // Elf_Phdr ph;
   ramdisk_read(&elf, 0, sizeof(Elf_Ehdr));
   printf("%s\n", elf.e_ident);
   printf("elf.e_type: %d\n", (int)elf.e_type);
   printf("elf.e_machine: %d\n", (int)elf.e_machine);
   printf("elf.e_entry: %d\n", (int)elf.e_entry);
   printf("elf.e_phoff: %d\n", (int)elf.e_ehsize);
-  ramdisk_read(&ph, sizeof(Elf_Ehdr), sizeof(Elf_Phdr));
+  // ramdisk_read(&ph, sizeof(Elf_Ehdr), sizeof(Elf_Phdr));
   
   assert(elf.e_ident[0] == 0x7f &&
          elf.e_ident[1] == 0x45 &&
          elf.e_ident[2] == 0x4c &&
          elf.e_ident[3] == 0x46);
-  printf("ph.p_type: %d\n", ph.p_type);
-  assert(ph.p_flags == (PF_X | PF_W | PF_R));
+  // printf("ph.p_type: %d\n", ph.p_type);
+  // assert(ph.p_flags == (PF_X | PF_W | PF_R));
   // ramdisk_read(pf + VirtualAddress, start_of_load,Memsize);
   return 0;
 }
