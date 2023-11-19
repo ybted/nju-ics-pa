58bfffc0ca70c6a0fb649f6adf086aa6755e83f7
tracer-ics2021 <tracer@njuics.org>
2023-11-19 16:58:44 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: da5e9a6afca24a1bb3d0121822bd7ad1   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  16:58:44 up 8 days, 36 min,  1 user,  load average: 0.23, 0.16, 0.10

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,6 +16,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(&elf, SEEK_SET, sizeof(Elf_Ehdr));
   printf("%s\n", elf.e_ident);
   printf("elf.e_type: %d\n", (int)elf.e_type);
+  printf("elf.e_machine: %d\n", (int)elf.e_machine);
   printf("elf.e_phoff: %d\n", (int)elf.e_phoff);
   ramdisk_read(&ph, SEEK_SET + elf.e_phoff , sizeof(Elf_Phdr));
   
