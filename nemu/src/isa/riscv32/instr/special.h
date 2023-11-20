b5dfe540c1fa891e7adc9b64c203e40db83b4b93
tracer-ics2021 <tracer@njuics.org>
2023-11-20 17:30:40 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: c0f089aa95ed4a1e94a109df0ae3576a   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  17:30:40 up 10:16,  1 user,  load average: 0.17, 0.25, 0.27

--- nemu/src/isa/riscv32/instr/special.h
+++ nemu/src/isa/riscv32/instr/special.h
@@ -17,7 +17,7 @@ def_EHelper(ecall) {
 def_EHelper(mret) {
   // printf("mret!!\n");
   // if (cpu.mcause == 1 || cpu.mcause == 2)
-    rtl_j(s, cpu.mepc + 4);
+    rtl_j(s, cpu.mepc);
   // else 
   //   rtl_j(s, cpu.mepc);
 }
