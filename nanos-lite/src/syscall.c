b5a924e3360fb7a1ee351236c4f4a1a444d9d9aa
tracer-ics2021 <tracer@njuics.org>
2023-11-21 11:44:35 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: d822949af08d4848818210e96e4ae9db   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  11:44:35 up  1:28,  1 user,  load average: 2.45, 2.03, 1.19

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,16 +1,25 @@
 #include <common.h>
 #include "syscall.h"
+void SYS_yield(Context *c) {
+  yield();
+  c->GPRx = 0;
+}
+
+void SYS_exit(Context *c) {
+  printf("EXIT!\n");
+  c->GPRx = 0;
+  halt(0);
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   switch (a[0]) {
     case 0:
-      printf("EXIT!\n");
-      c->GPRx = 0;
+      SYS_exit(c);
       break;
     case 1:
-      yield();
-      c->GPRx = 0;
+      SYS_yield(c);
       break;
   }
 }
