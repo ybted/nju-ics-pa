13fe1b4d6527d355f674e8f4f9d4430739795215
tracer-ics2021 <tracer@njuics.org>
2023-11-21 16:28:47 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: d822949af08d4848818210e96e4ae9db   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  16:28:47 up  6:12,  1 user,  load average: 0.47, 0.54, 0.44

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -8,7 +8,8 @@ int main() {
   while (1) {
     j ++;
     if (j == 10000) {
-      write(1, "Hello World from Navy-apps for the 3th time!\n", 46);
+      printf("Hello World from Navy-apps for the %dth time!\n", i);
+      i ++;
       j = 0;
     }
   }
