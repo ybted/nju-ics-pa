6fa2de107db3e969e51ea81b285170c1c1f30c85
tracer-ics2021 <tracer@njuics.org>
2023-11-21 16:02:31 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: d822949af08d4848818210e96e4ae9db   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  16:02:31 up  5:46,  1 user,  load average: 0.23, 0.20, 0.18

--- abstract-machine/am/include/arch/riscv32-nemu.h
+++ abstract-machine/am/include/arch/riscv32-nemu.h
@@ -8,9 +8,9 @@ struct Context {
 };
 
 #define GPR1 gpr[17] // a7
-#define GPR2 gpr[10] // a1
-#define GPR3 gpr[11] // a2
-#define GPR4 gpr[12] // a3
+#define GPR2 gpr[10] // a0
+#define GPR3 gpr[11] // a1
+#define GPR4 gpr[12] // a2
 #define GPRx gpr[10] // a0
 
 #endif
