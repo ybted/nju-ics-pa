862b6b8551212d443611b44bf206320cb020d3d9
tracer-ics2021 <tracer@njuics.org>
2023-11-22 20:22:01 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 513eefcd98104e98b97659b40384812e   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  20:22:01 up  1:32,  1 user,  load average: 0.20, 0.13, 0.09

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <NDL.h>
-
+#include <assert.h>
 int main() {
   NDL_Init(0);
   while (1) {
     char buf[64];
-    printf("1");
+    assert(0);
     if (NDL_PollEvent(buf, sizeof(buf))) {
       printf("receive event: %s\n", buf);
     }
