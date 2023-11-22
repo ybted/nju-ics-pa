a5fa6083a6769a37ee3b9eff26022d0ea16045fb
tracer-ics2021 <tracer@njuics.org>
2023-11-22 11:31:09 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 2099a7c4f35647e2990a19ef5684625f   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  11:31:09 up  2:59,  1 user,  load average: 0.14, 0.10, 0.09

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -55,7 +55,7 @@ int fs_open(const char *pathname, int flags, int mode)
 size_t fs_read(int fd, void *buf, size_t len) 
 {
   Finfo file = file_table[fd];
-  assert(file.disk_offset + file.open_offset + len < file.disk_offset + file.size);
+  assert(file.disk_offset + file.open_offset + len <= file.disk_offset + file.size);
   int read_size = ramdisk_read(buf, file.disk_offset + file.open_offset, len);
   file_table[fd].open_offset += read_size;
   return read_size;
@@ -79,7 +79,7 @@ size_t fs_lseek(int fd, size_t offset, int whence)
 size_t fs_write(int fd, const void *buf, size_t len)
 {
   Finfo file = file_table[fd];
-  assert(file.disk_offset + file.open_offset + len < file.disk_offset + file.size);
+  assert(file.disk_offset + file.open_offset + len <= file.disk_offset + file.size);
   int write_size = ramdisk_write(buf, file.disk_offset + file.open_offset, len);
   file_table[fd].open_offset += write_size;
   return write_size;
