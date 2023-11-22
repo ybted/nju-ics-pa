7094f0cb08d2f125f41deeaae406d06145e754d6
tracer-ics2021 <tracer@njuics.org>
2023-11-22 20:30:46 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 513eefcd98104e98b97659b40384812e   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  20:30:46 up  1:41,  1 user,  load average: 0.27, 0.31, 0.20

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,7 +25,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   
   int keycode = io_read(AM_INPUT_KEYBRD).keycode; 
   bool keydown = io_read(AM_INPUT_KEYBRD).keydown; 
-  printf("events read!\n");
+  printf("%d\n", keydown);
   if (keydown) {
     
     memcpy(buf, &keyname[keycode], sizeof(keyname[keycode]));
