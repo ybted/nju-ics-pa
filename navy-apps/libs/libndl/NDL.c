561091b45454a69ace1b20c49ba6a837c3253b58
tracer-ics2021 <tracer@njuics.org>
2023-11-22 20:19:27 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 513eefcd98104e98b97659b40384812e   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  20:19:27 up  1:29,  1 user,  load average: 0.10, 0.06, 0.07

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -17,7 +17,9 @@ uint32_t NDL_GetTicks() {
 int NDL_PollEvent(char *buf, int len) {
   int fd = fopen("/dev/events", "r+");
   int size = read(fd, buf, len);
-  return size != 0;
+  if (size == 0) 
+    return 1;
+  return 0;
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
