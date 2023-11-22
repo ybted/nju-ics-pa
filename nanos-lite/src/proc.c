80bf9b0f753630583a4864c2197f01b8b7812e84
tracer-ics2021 <tracer@njuics.org>
2023-11-22 20:06:18 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 513eefcd98104e98b97659b40384812e   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  20:06:18 up  1:16,  1 user,  load average: 0.39, 0.21, 0.13

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -21,7 +21,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   switch_boot_pcb();
-  naive_uload( NULL, "/bin/timer-test");
+  naive_uload( NULL, "/bin/event-test");
   Log("Initializing processes...");
   
   // load program here
