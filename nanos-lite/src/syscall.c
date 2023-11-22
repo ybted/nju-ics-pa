43cc22a0a65de878bf4b6ed1f3277b3b807818e5
tracer-ics2021 <tracer@njuics.org>
2023-11-22 11:30:41 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 2099a7c4f35647e2990a19ef5684625f   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  11:30:41 up  2:58,  1 user,  load average: 0.03, 0.07, 0.08

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,8 +20,12 @@ void sys_write(Context *c) {
     for (int i = 0; i < count; i ++) {
       putch(*(char *)(buf+i));
     }
+    c->GPRx = count;
   }
-  c->GPRx = count;
+  else {
+    c->GPRx = fs_write(fd, buf, count);
+  }
+  
 }
 
 void sys_brk(Context *c) {
@@ -33,6 +37,21 @@ void sys_open(Context* c) {
   int flags = (int)c->GPR3;
   int mode = (int)c->GPR4;
   c->GPRx = fs_open(pathname, flags, mode);
+}
+
+void sys_read(Context* c) {
+  int fd = c->GPR2;
+  void* buf = (void *)c->GPR3;
+  int count = c->GPR4;
+  c->GPRx = fs_read(fd, buf, count);
+}
+
+void sys_lseek(Context* c) {
+  c->GPRx = fs_lseek(c->GPR2, c->GPR3, c->GPR4);
+}
+
+void sys_close(Context* c) {
+  c->GPRx = fs_close(c->GPR2);
 }
 
 void do_syscall(Context *c) {
@@ -56,6 +75,15 @@ void do_syscall(Context *c) {
       break;
     case SYS_open:
       sys_open(c);
+      break;
+    case SYS_read:
+      sys_read(c);
+      break;
+    case SYS_lseek:
+      sys_lseek(c);
+      break;
+    case SYS_close:
+      sys_close(c);
       break;
   }
 }
