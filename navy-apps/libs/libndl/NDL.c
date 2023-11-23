ee559f83d711c181243c46c87194b4d2fe5f9073
tracer-ics2021 <tracer@njuics.org>
2023-11-23 17:37:03 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 86c2b41576034c11840e154b50fb086a   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  17:37:03 up  2:30,  1 user,  load average: 0.27, 0.46, 0.38

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -44,7 +44,7 @@ void NDL_OpenCanvas(int *w, int *h) {
   int fd = open("/proc/dispinfo", O_RDONLY);
   char buffer[100];
   int size = read(fd, (void *)buffer, 100);
-  printf("size: %d\n", size);
+  printf("buf: %s\n", buffer);
   sscanf(buffer, "WIDTH:%dHEIGHT:%d", &w, &h);
   printf("w: %d h: %d\n", w, h); 
 }
