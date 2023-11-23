99e7e38883d4ff76e02bccd297a82c73ae9da303
tracer-ics2021 <tracer@njuics.org>
2023-11-23 14:23:02 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: e1b17d88d9e14e099996d7e87ee96e3b   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  14:23:02 up 15 min,  1 user,  load average: 0.24, 0.19, 0.10

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,14 +4,8 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t input = inl(KBD_ADDR);
-  if (input)
-    printf("input: %d\n", input);
-  if (input != AM_KEY_NONE){
-    kbd->keydown = KEYDOWN_MASK & input;
-    kbd->keycode = input & (~KEYDOWN_MASK);
-  }else {
-    kbd->keydown = false;
-    kbd->keycode = AM_KEY_NONE;
-  }
+  int k = AM_KEY_NONE;
+  k = inl(KBD_ADDR);
+  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
+  kbd->keycode = k & ~KEYDOWN_MASK;
 }
