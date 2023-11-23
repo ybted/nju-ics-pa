b2939c27dd9cda79e7ac262b4872faec92cf6a60
tracer-ics2021 <tracer@njuics.org>
2023-11-23 14:33:56 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: e1b17d88d9e14e099996d7e87ee96e3b   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  14:33:56 up 26 min,  1 user,  load average: 0.06, 0.09, 0.08

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -70,8 +70,9 @@ static uint32_t *i8042_data_port_base = NULL;
 static void i8042_data_io_handler(uint32_t offset, int len, bool is_write) {
   assert(!is_write);
   assert(offset == 0);
-  printf("CALL i8042\n");
   i8042_data_port_base[0] = key_dequeue();
+  if (i8042_data_port_base[0])
+    printf("i8042: %d\n", i8042_data_port_base[0]);
 }
 
 void init_i8042() {
