64388e0963d03866a552d3d9837b4cef61087895
tracer-ics2021 <tracer@njuics.org>
2023-11-23 17:03:41 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 86c2b41576034c11840e154b50fb086a   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  17:03:41 up  1:57,  1 user,  load average: 0.30, 0.30, 0.23

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -33,7 +33,7 @@ char *strcat(char *dst, const char *src) {
 
 int strcmp(const char *s1, const char *s2) {
   // printf("test: %s\n", s1);
-  printf("s1: %s s2: %s\n", s1, s2);
+  // printf("s1: %s s2: %s\n", s1, s2);
   while (*s1 && (*s1 == *s2)) 
   {
     
