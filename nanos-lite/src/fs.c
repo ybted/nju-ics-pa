43c76675dd3330e172cf5f87e730ef11d8809c11
tracer-ics2021 <tracer@njuics.org>
2023-11-24 16:57:25 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 9a3f4a73b91e44c5a561c9af06274f5b   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  16:57:25 up  1:13,  1 user,  load average: 0.44, 0.24, 0.14

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -47,6 +47,7 @@ void init_fs() {
 int fs_open(const char *pathname, int flags, int mode)
 {
   int i = 0;
+  printf("pathname: %s\n", pathname);
   int len = sizeof(file_table) / sizeof(file_table[0]);
   for (; i < len; i ++)
   {
