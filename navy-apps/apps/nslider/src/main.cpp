7dedd34b0d666e2471d12d15bf15f00ff4a302fe
tracer-ics2021 <tracer@njuics.org>
2023-11-24 16:56:41 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 9a3f4a73b91e44c5a561c9af06274f5b   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  16:56:41 up  1:12,  1 user,  load average: 0.61, 0.24, 0.14

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -47,7 +47,7 @@ void next(int rep) {
 int main() {
   SDL_Init(0);
   SDL_Surface *screen = SDL_SetVideoMode(W, H, 32, SDL_HWSURFACE);
-
+  printf("Nslider!\n");
   int rep = 0, g = 0;
 
   render();
