6f11e764a29745ff6eaf2bd1d5193f14a79fa19e
tracer-ics2021 <tracer@njuics.org>
2023-11-29 16:02:34 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 937f7c13d77b4bff9909ebd1cb61cbdd   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  16:02:34 up  1:51,  1 user,  load average: 0.81, 0.49, 0.26

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -23,7 +23,6 @@ static void sh_prompt() {
 }
 
 static void sh_handle_cmd(const char *cmd) {
-  printf("cmd: %s\n", cmd);
   execve(cmd, NULL, NULL);
 }
 
--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -100,6 +100,7 @@ int _gettimeofday(struct timeval *tv, struct timezone *tz) {
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
+  printf("fname: %s\n", fname);
   return _syscall_(SYS_execve, fname, argv, envp);
 }
 
