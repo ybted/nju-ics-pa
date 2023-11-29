fb40a1c40c49b40bd4c0963c43669d587501e298
tracer-ics2021 <tracer@njuics.org>
2023-11-29 12:53:04 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 173f3a9f7e2541d0881dc29a7646ec2a   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  12:53:04 up 1 day, 20:33,  1 user,  load average: 0.17, 0.19, 0.10

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -24,16 +24,32 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  if (!ctl->sync && (w == 0 || h == 0)) return;
-  uint32_t *pixels = ctl->pixels;
+  // int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  // if (!ctl->sync && (w == 0 || h == 0)) return;
+  // uint32_t *pixels = ctl->pixels;
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // int screen_w = (inl(VGACTL_ADDR) >> 16 )& 0xffff;
+  // for (int i = y; i < y + h; i ++) {
+  //   for (int j = x; j < x + w; j ++) {
+  //     fb[j + i * screen_w] = pixels[ j -x + (i - y) * w];
+  //   }
+  // }
+  // if (ctl->sync) {
+  //   outl(SYNC_ADDR, 1);
+  // }
+  int win_weight = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
+
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  int screen_w = (inl(VGACTL_ADDR) >> 16 )& 0xffff;
-  for (int i = y; i < y + h; i ++) {
-    for (int j = x; j < x + w; j ++) {
-      fb[j + i * screen_w] = pixels[ j -x + (i - y) * w];
+  uint32_t *pi = (uint32_t *)(uintptr_t)ctl->pixels;
+  // printf("(x:%d, y:%d) w=%d, h=%d", ctl->x, ctl->y, ctl->w, ctl->h);
+  for (int i = 0; i < ctl->h; ++i){
+    for (int j = 0; j < ctl->w; ++j){
+      fb[(ctl->y) * win_weight + i * win_weight + ctl->x + j] = pi[i * (ctl->w) + j];
+      //真t娘的奇怪
+      //outl((uint32_t)(fb + ctl->y * win_weight + i * win_weight + ctl->x + j), pi[i * (ctl->w) + j]);
     }
   }
+  
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
