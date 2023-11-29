7c4a9c5d152e4c6c3690408f94cca11e0b112b1b
tracer-ics2021 <tracer@njuics.org>
2023-11-29 13:18:47 +0800

    >  compile 20330070  Static hostname: DESKTOP-48LRV1H        Icon name: computer-container          Chassis: container       Machine ID: f04e32c1eb114443bb2a74a1efa6099c          Boot ID: 173f3a9f7e2541d0881dc29a7646ec2a   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  13:18:48 up 1 day, 20:58,  1 user,  load average: 0.08, 0.25, 0.25

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -25,7 +25,6 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  printf("fbdraw\n");
   if (!ctl->sync && (w == 0 || h == 0)) return;
   uint32_t *pixels = ctl->pixels;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
@@ -35,8 +34,9 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
       fb[j + i * screen_w] = pixels[ j -x + (i - y) * w];
     }
   }
+  if (ctl->sync) {
     outl(SYNC_ADDR, 1);
-  
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
